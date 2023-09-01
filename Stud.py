ecd5f840b5a1fa09b2837cb781ae0c965237195b
natnaelderibe <natnael.deribe@aau.edu.et>
2023-09-01 13:00:27 +0300

    i add e to name and age

--- Stud.py
+++ Stud.py
@@ -1,9 +1,9 @@
 name = input("Student Name = ")
 age = input("Student Age = ")
 
-def Stud(namee,agee):
-    print("Name = " + namee)
-    print("Age =" + agee) 
+def Stud(nameee,ageee):
+    print("Name = " + nameee)
+    print("Age =" + ageee) 
 
 
 Stud(name,age)
